50a7734d09ebe7f9b8d47007da0da62f306439b7
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-09-25 08:37:28 +0100

    grid design

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+//const chalk = require("chalk");
+const keypress = require("keypress");
+
+let displayBoard = (board) => {
+  var buffer = "";
+  for (var i = 0; i < board.length; i++) {
+    for (var x = 0; x < board[i].length; x++) {
+      buffer += board[i][x];
+    }
+    buffer += "\n";
+  }
+  console.log(buffer);
+};
+
+let createBoard = (width, height) => {
+  board = [];
+  row = [];
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
+      row.push("[ ]");
+    }
+    board.push(row);
+    row = [];
+  }
+  return board;
+};
+
+let output = createBoard(10, 10);
+let xCoord = 0;
+let yCoord = 0;
+output[xCoord][yCoord] = `[X]`;
+console.log(
+  "Acceptable inputs are\n-u: move the cursor upwards 1 block\n-d: move the cursor downwards 1 block\n-l: move the cursor left 1 block\n-r: move the cursor right 1 block\n-p: print the cursor path\n-e: exit the application"
+);
+displayBoard(output);
+
 let position = 0;
 let pathStorageArray = [position];
 
